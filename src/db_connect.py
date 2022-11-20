17730dedf3d18aae87ed9e601c65dc893c3c062e
Radoslav Elias <radoslave0@gmail.com>
2022-11-20 09:06:19 +0100

    added config file, fixed up oracle connection

--- src/db_connect.py
+++ src/db_connect.py
@@ -2,21 +2,33 @@ import cx_Oracle
 from sqlalchemy import types, create_engine
 import pandas as pd
 import downloadDB
+import configparser
 
 
 def connect_to_oracle_pandas(name,password):
-    return create_engine('oracle://{}:{}@gort.fit.vutbr.cz:1521/orclpdb'.format(name,password))
+    dsn = cx_Oracle.makedsn(
+        'gort.fit.vutbr.cz', 
+        1521,
+        service_name='orclpdb'
+    )
+    return create_engine(f"oracle+cx_oracle://{name}:{password}@{dsn}")
 
 def connect_to_oracle_cursor(name,password):
     conn = cx_Oracle.connect('{}/{}@gort.fit.vutbr.cz:1521/orclpdb'.format(name,password))
     return conn.cursor()
 
 if __name__ == '__main__':
-    conn = connect_to_oracle_pandas('xlogin','password')
+    config = configparser.ConfigParser()
+    config.read('config.conf')
+    name = config['oracle_sql']['name']
+    password = config['oracle_sql']['password']
     
-    print(conn)
+    engine = connect_to_oracle_pandas(name, password)
     df = pd.read_csv('pdb_dataset.csv')
-    df.to_sql('products', conn, if_exists='replace')
-    
-    #RawData= pd.read_sql_query("select * from products", conn)
-    #RawData.head()
+
+    with engine.connect() as connection:
+        df.to_sql('products', connection, if_exists='replace')
+
+    with engine.connect() as connection:
+        rawData = pd.read_sql_query("SELECT * FROM products", connection)
+        print(rawData.head())
