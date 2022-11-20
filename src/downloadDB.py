8b218df4d8382c4c8992e4f98b888b24d9336df5
xjahod06 <jahoda.vojta@gmail.com>
2022-11-20 19:14:50 +0100

    updated data preload, product_id -> _id for mongoDB and removed null values

--- src/downloadDB.py
+++ src/downloadDB.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def random_dates(start, end, n, unit='D', seed=None):
-    if not seed:  # from piR's answer
+    if not seed:
         np.random.seed(0)
 
     ndays = (end - start).days + 1
@@ -14,13 +14,26 @@ def random_dates(start, end, n, unit='D', seed=None):
 def load_df():
     df = pd.read_csv(
         'https://query.data.world/s/7ywhs3irjz3jwexgfc7idx6pmz62kl')
+    
+    #replacing index with _id for mongo
+    df['_id'] = df['product_id']
+    df = df.set_index('_id')
+    
+    #cleaning data
+    df = df.drop(['url', 'uniq_id', 'scraped_at', 'availability','product_id'], axis=1)
+    df['sku'].fillna(0,inplace=True)
+    df['gtin13'].fillna(0,inplace=True)
+    
+    #randomizing some values
     df['in_stock'] = random.choices([0, 1, 2, 3], k=len(df))
-    df = df.drop(['url', 'uniq_id', 'scraped_at', 'availability'], axis=1)
     start = pd.to_datetime('2020-01-01')
     end = pd.to_datetime('2022-10-01')
     df['added_at'] = random_dates(start, end, len(df))
+    
     return df
 
 
 if __name__ == "__main__":
-    load_df().to_csv('pdb_dataset.csv')
+    df = load_df()
+    print(df.head())
+    df.to_csv('pdb_dataset.csv')
