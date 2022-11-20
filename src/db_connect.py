8b918328c56c91cde3ac4a61e934d89b31885191
xjahod06 <jahoda.vojta@gmail.com>
2022-11-20 14:49:06 +0100

    added mongo container

--- src/db_connect.py
+++ src/db_connect.py
@@ -3,9 +3,10 @@ from sqlalchemy import types, create_engine
 import pandas as pd
 import downloadDB
 import configparser
+from pymongo import MongoClient
+import json
 
-
-def connect_to_oracle_pandas(name,password):
+def connect_to_oracle(name,password):
     dsn = cx_Oracle.makedsn(
         'gort.fit.vutbr.cz', 
         1521,
@@ -17,18 +18,40 @@ def connect_to_oracle_cursor(name,password):
     conn = cx_Oracle.connect('{}/{}@gort.fit.vutbr.cz:1521/orclpdb'.format(name,password))
     return conn.cursor()
 
+def df_to_sql_db(engine,df):
+    with engine.connect() as connection:
+        df.to_sql('products', connection, if_exists='replace')
+
+def get_oracle_data(engine,show=True):
+    with engine.connect() as connection:
+        rawData = pd.read_sql_query("SELECT * FROM products", connection)
+        print(rawData.head()) if show == True else None
+        return rawData
+
+def connect_to_mongo(name,password):
+    return MongoClient(config['mongo_db']['uri'].format(name,password))['pdb']
+
+def df_to_mongo(db,df):
+    db.products.insert_many(df.to_dict('records'), ordered=False)
+    
+def get_mongo_data(db,show=True):
+    df = pd.DataFrame(list(mongoDb.myCollection.find()))
+    print(df.head()) if show == True else None
+    return df
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.conf')
     name = config['oracle_sql']['name']
     password = config['oracle_sql']['password']
     
-    engine = connect_to_oracle_pandas(name, password)
+    engine = connect_to_oracle(name, password)
     df = pd.read_csv('pdb_dataset.csv')
+    
+    df_to_sql_db(engine,df) 
+    get_oracle_data(engine)
 
-    with engine.connect() as connection:
-        df.to_sql('products', connection, if_exists='replace')
-
-    with engine.connect() as connection:
-        rawData = pd.read_sql_query("SELECT * FROM products", connection)
-        print(rawData.head())
+    mongoDb = connect_to_mongo(config['mongo_db']['name'],config['mongo_db']['password']) 
+    
+    df_to_mongo(mongoDb,df)
+    get_mongo_data(mongoDb)
