491ae32acb897ebc02da20928a526b60041031df
xjahod06 <jahoda.vojta@gmail.com>
2022-11-24 17:40:54 +0100

    added crud operations for oracle DB workign with session engine and new models and schemas

---
+++ src/crud.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+import models
+import schemas
+from datetime import datetime
+
+def get_product(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+def get_products(db: Session ):             #, skip: int = 0, limit: int = 100):
+    return db.query(models.Product).all()   #.offset(skip).limit(limit).all()
+
+
+def create_product(db: Session, product: schemas.ProductCreate):
+    db_product = models.Product(**product.dict(), added_at=datetime.now())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
