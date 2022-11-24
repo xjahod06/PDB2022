689b1b85af62d438992b474f6226a238646a99d6
xjahod06 <jahoda.vojta@gmail.com>
2022-11-24 22:20:36 +0100

    fixed DB model with to properly create DB

--- src/crud.py
+++ src/crud.py
@@ -4,14 +4,14 @@ import schemas
 from datetime import datetime
 
 def get_product(db: Session, product_id: int):
-    return db.query(models.Product).filter(models.Product.id == product_id).first()
+    return db.query(models.Product).filter(models.Product._id == product_id).first()
 
 def get_products(db: Session ):             #, skip: int = 0, limit: int = 100):
     return db.query(models.Product).all()   #.offset(skip).limit(limit).all()
 
 
 def create_product(db: Session, product: schemas.ProductCreate):
-    db_product = models.Product(**product.dict(), added_at=datetime.now())
+    db_product = models.Product(**product.dict())
     db.add(db_product)
     db.commit()
     db.refresh(db_product)
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DATETIME #Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime,Identity #Boolean, ForeignKey
+from datetime import datetime
 #from sqlalchemy.orm import relationship
 
 from db_connect import Base
@@ -6,14 +7,14 @@ from db_connect import Base
 class Product(Base):
     __tablename__ = "products"
     
-    _id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    images = Column(String, default=None)
-    description = Column(String, default=None)
+    _id = Column(Integer, Identity(start=1), primary_key=True,) #index=True,
+    title = Column(String(50))
+    images = Column(String(50), default=None)
+    description = Column(String(50), default=None)
     sku = Column(Integer)
     gtin13 = Column(Integer)
-    brand = Column(String)
+    brand = Column(String(50))
     price = Column(Integer)
-    currency = Column(String)
+    currency = Column(String(50))
     in_stock = Column(Integer)
-    added_at = Column(DATETIME)
+    added_at = Column(DateTime, default=datetime.utcnow)
