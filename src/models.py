63126109f7714c53c163c39747fb0e9224ccddf9
xjahod06 <jahoda.vojta@gmail.com>
2022-11-24 22:42:24 +0100

    updated models with text and unitext type

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime,Identity #Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Identity, Text, UnicodeText #ForeignKey
 from datetime import datetime
 #from sqlalchemy.orm import relationship
 
@@ -8,13 +8,13 @@ class Product(Base):
     __tablename__ = "products"
     
     _id = Column(Integer, Identity(start=1), primary_key=True,) #index=True,
-    title = Column(String(50))
-    images = Column(String(50), default=None)
-    description = Column(String(50), default=None)
+    title = Column(String(64))
+    images = Column(Text, default=None)
+    description = Column(UnicodeText, default=None)
     sku = Column(Integer)
     gtin13 = Column(Integer)
-    brand = Column(String(50))
+    brand = Column(String(20))
     price = Column(Integer)
-    currency = Column(String(50))
+    currency = Column(String(3))
     in_stock = Column(Integer)
     added_at = Column(DateTime, default=datetime.utcnow)
