41944159a88024514d388d5de25087ed9316b07f
xjahod06 <jahoda.vojta@gmail.com>
2022-11-28 11:47:05 +0100

    CQRS for post endpoint

--- src/api.py
+++ src/api.py
@@ -8,15 +8,15 @@ from datetime import datetime
 from db_connect import *
 import configparser
 
-
 config = configparser.ConfigParser()
 config.read('config.conf')
 mongoDB = async_connect_to_mongo(config['mongo_db']['name'],config['mongo_db']['password'])
+mongoDBSync = connect_to_mongo(config['mongo_db']['name'],config['mongo_db']['password'])
 
 engine = connect_to_oracle(config['oracle_sql']['name'],config['oracle_sql']['password'])
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -29,12 +29,19 @@ def get_db():
         db.close()
 
 
+def create_product_mongo(product: dict):
+    return mongoDBSync.products.insert_one(product)
+
 @app.post("/oracle/products/", response_model=schemas.Product)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
     db_product = crud.create_product(db=db, product=product)
     if db_product is None:
         raise HTTPException(status_code=400, detail="this product is already in DB")
-    return db_product
+    else:
+        res = (create_product_mongo(db_product.as_dict()))
+        if (res.inserted_id != db_product._id):
+            raise HTTPException(status_code=500, detail="sync between mongo and oracle failed")
+        return db_product
 
 
 @app.get("/oracle/products/{product_id}", response_model=schemas.Product)
